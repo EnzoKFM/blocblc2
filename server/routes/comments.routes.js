a7c03b8b8775a33a80aa80d42710572ecbcf865a
EnzoKFM <enzo.kaufmann@outlook.fr>
2026-03-04 12:26:07 +0100

    feat: ajout d'un middleware pour sanitizer

--- server/routes/comments.routes.js
+++ server/routes/comments.routes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const commentsController = require('../controllers/comments.controller');
 const verifyToken = require('../middleware/auth');
+const sanitizeCommentMiddleware = require('../middleware/sanitizeCommentMiddleware')
 
-router.put('/:id', verifyToken, commentsController.updateComment);
+router.put('/:id', verifyToken, sanitizeCommentMiddleware, commentsController.updateComment);
 router.delete('/:id', verifyToken, commentsController.deleteComment);
 
 module.exports = router;
